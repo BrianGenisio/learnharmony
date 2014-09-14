8f6c69545606939696831690178de0eaabd8ad17
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-14 16:25:06 -0400

    Adding CodeMirror to the IDE

--- src/editor/editor.template.js
+++ src/editor/editor.template.js
@@ -1,8 +1,11 @@
 export var editorTemplate = `
 
 <div id="code-editor">
-  <textarea id="source-editor"></textarea>
-  <button id="run">Run!</button>
+  <textarea id="source-editor" class="form-control" rows="6">
+let arrowAlert = x => alert(x);
+arrowAlert('what?')
+  </textarea>
+  <button id="run" class="btn">Run!</button>
 </div>
 
 `;
