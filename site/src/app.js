97c2c2bd8c989733bf387278bb45fbb72946f994
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-13 22:12:09 -0400

    Getting the compiler abstracted for our use case.

--- site/src/app.js
+++ site/src/app.js
@@ -1,3 +1,5 @@
+import StringCompiler from 'src/editor/string-compiler';
+
 class App {
   constructor(message) {
     this.message = message;
@@ -11,3 +13,9 @@ class App {
 
 let app = new App('Hello, world!');
 app.greet();
+
+let compiler = new StringCompiler();
+var transpiled = compiler.transpile('let foo = x => x * x; console.log(foo(5));');
+
+console.log('running', transpiled);
+eval(transpiled);
