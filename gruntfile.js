ac899834e53df63ca59545cd2aed503c611be12c
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-14 15:31:39 -0400

    added a jshint watcher

--- gruntfile.js
+++ gruntfile.js
@@ -8,12 +8,29 @@ module.exports = function(grunt) {
         port: 8282,
         host: "127.0.0.1",
         ext: "html",
-        runInBackground: false
+        runInBackground: true
       }
+    },
+
+    watch: {
+      scripts: {
+        files: 'site/src/**/*.js',
+        tasks: ['jshint']
+      }
+    },
+
+    jshint: {
+      options: {
+        evil: true,
+        esnext: true
+      },
+      src: ['site/src/**/*.js', '!site/src/**/*.template.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-http-server');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['http-server:dev']);
+  grunt.registerTask('default', ['http-server:dev', 'watch']);
 }
