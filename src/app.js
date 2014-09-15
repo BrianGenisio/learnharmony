ce24fc172d93427e2f343d59c7979d755befe98a
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-15 17:24:31 -0400

    hijack the console.log to display it in the browser.  First pass

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
+import {appTemplate} from 'src/app.template';
 import Editor from 'src/editor/editor';
+import Console from 'src/console/console';
+
+$('#app-container').html(appTemplate);
 
 let editor = new Editor();
-editor.render($('#app-container'));
+editor.render($('#app-container .editor-container'));
+
+let console = new Console();
+console.render($('#app-container .console-container'));
---
+++ src/app.template.js
@@ -0,0 +1,8 @@
+export var appTemplate = `
+
+<div class="row">
+  <div class="col-md-6 editor-container"></div>
+  <div class="col-md-6 console-container"></div>
+</div>
+
+`;
