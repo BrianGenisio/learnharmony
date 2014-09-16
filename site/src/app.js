28833e82f5ca42ca6439c3308f1722466cc32442
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-15 20:41:49 -0400

    adding super-simple router

--- site/src/app.js
+++ site/src/app.js
@@ -1,6 +1,7 @@
 import {appTemplate} from 'src/app.template';
 import Editor from 'src/editor/editor';
 import Console from 'src/console/console';
+import Router from 'src/router/router';
 
 $('#app-container').html(appTemplate);
 
@@ -9,3 +10,9 @@ editor.render($('#app-container .editor-container'));
 
 let console = new Console();
 console.render($('#app-container .console-container'));
+
+let router = new Router();
+router.route('/', () => window.console.log('home'));
+router.route('about', () => window.console.log('about'));
+router.route('contact', () => window.console.log('contact'));
+router.listen();
