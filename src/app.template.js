0694ecf65f33e9dd715799b2aec3e31b23d8677c
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-16 22:47:30 -0400

    re-working to let the router control the page content.

--- src/app.template.js
+++ src/app.template.js
@@ -1,6 +1,10 @@
 export var appTemplate = `
 
-<div class="row">
+<h2 class="heading"></h2>
+
+<div class="intro well"></div>
+
+<div class="row editor">
   <div class="col-md-6 editor-container"></div>
   <div class="col-md-6 console-container"></div>
 </div>
--- src/editor/editor.template.js
+++ src/editor/editor.template.js
@@ -1,10 +1,7 @@
 export var editorTemplate = `
 
 <div id="code-editor">
-  <textarea id="source-editor" class="form-control" rows="6">
-let arrowAlert = x => console.log(x);
-arrowAlert('what?')
-  </textarea>
+  <textarea id="source-editor" class="form-control" rows="6"></textarea>
   <button id="run" class="btn">Run!</button>
 </div>
 
