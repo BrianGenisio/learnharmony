cd04ae6711963524021932802e2cdc831ecd279b
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-18 23:49:49 -0400

    handle code highlighting

--- src/pages/lessons/arrow-functions.page.js
+++ src/pages/lessons/arrow-functions.page.js
@@ -3,5 +3,5 @@ export var page = {
     "navGroup": ".lessons",
     "heading": "Arrow Functions",
     "code": "var squares = [1, 2, 3].map(x => x * x);\nconsole.log('squares: ', squares);",
-    "intro": "<p>Talk about arrow functions</p>\n\n<div class=\"highlight\"><pre lang=\"javascript\">function logMe() {\n    console.log('testing code');\n}\n</pre></div>"
+    "intro": "<p>Talk about arrow functions</p>\n<pre><code class=\"lang-javascript\"><span class=\"hljs-function\"><span class=\"hljs-keyword\">function</span> <span class=\"hljs-title\">logMe</span><span class=\"hljs-params\">()</span> </span>{\n    <span class=\"hljs-built_in\">console</span>.log(<span class=\"hljs-string\">'testing code'</span>);\n}\n</code></pre>\n"
 };
