ece324184c1d56ca174dd656368f48f23fed5c0d
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-21 22:04:06 -0400

    putting in some footer text and link

--- site/src/app.template.js
+++ site/src/app.template.js
@@ -1,12 +1,17 @@
 export var appTemplate = `
 
-<h2 class="heading"></h2>
+<h2 class="heading col-md-12"></h2>
 
-<div class="intro"></div>
+<div class="intro col-md-12"></div>
 
-<div class="row editor">
+<div class="editor">
   <div class="col-md-6 editor-container"></div>
   <div class="col-md-6 console-container"></div>
+</div>
+
+<div class="footer">
+  <div class="col-md-10 next-text"></div>
+  <div class="col-md-2 next-link"><a href="#"><span class="text">Next</span> &gt;&gt;</a></div>
 </div>
 
 `;
