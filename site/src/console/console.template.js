46476322d7da2bea91646dadda4e4b1b3e815319
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-25 19:49:50 -0400

    added clear functionality to the UI console

---
+++ site/src/console/console.template.js
@@ -0,0 +1,12 @@
+export var consoleTemplate = `
+
+<div id="console">
+  <div class="form-group output">
+    <pre></pre>
+  </div>
+  <div class="form-group">
+    <button id="clear" class="btn btn-default btn-block form-control">Clear Log</button>
+  </div>
+</div>
+
+`;
