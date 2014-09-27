ff4b9a4186763cb58941dbd0bbe6ca004b312e9d
Brian Genisio <BrianGenisio@Gmail.com>
2014-09-26 22:36:55 -0400

    adding comments to pages

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -5,6 +5,7 @@ export var page = {
     "next": "lessons/arrow-functions",
     "nextText": "Let's get started with our first lession -- Arrow Functions",
     "heading": "Learn ES6 (Harmony) Now!",
+    "hideComments": true,
     "code": "let futureJavascript = () => {\n    let ES6 = 'awesome';\n    console.log(`The future of JavaScript is ${ES6}`);\n};\n\nfutureJavascript();",
     "intro": "<p>Welcome to LearnHarmony.org.  This site is designed to help you learn the new features of ECMAScript (primarily ES6 for now) through examples and exploration.  This site is in the early phases, so for now, just play along in the editor.  It uses Traceur, so any features that Traceur supports should work here.  Write to the console, have some fun.</p>\n<p>Over the next several weeks, I will start to put up micro-lessons designed to teach individual features.  Come back often! I expect it to be fun.</p>\n"
 };
