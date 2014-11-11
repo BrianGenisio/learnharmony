f90ee27dcb7439a74a6f6ac545b3facfc41509f0
Brian Genisio <BrianGenisio@Gmail.com>
2014-11-11 13:06:29 -0500

    updating some things

---
+++ site/LearnHarmony.roadmap.md
@@ -0,0 +1,32 @@
+# Lessons
+ 
+- Rest Parameters
+- Spread Parameters
+- Modules
+- Generators
+- For/Of
+- Iterators
+- Two-way iterators
+- Coroutines
+
+# New Stuff
+
+- Promise
+- New String Stuff (startsWith, endsWith, contains, repeat)
+- New Array Stuff (fill, find, findIndex, Array.from, keys/entries/values)
+- Maps
+- Private Fields
+- Proxies
+
+
+# Transpilers/Polyfills
+
+- Traceur
+- TypeScript
+- es6-transpiler
+- regenerator
+- es6-shim
+- es6-collections
+- es6-module-loader
+- harmony-reflect (proxies and reflect)
+
--- site/src/pages/processed/lessons/classes-extended.page.js
+++ site/src/pages/processed/lessons/classes-extended.page.js
@@ -1,7 +1,7 @@
 export var page = {
     "title": "Classes -- Extended",
     "navGroup": ".lessons",
-    "heading": "Classes",
+    "heading": "Classes - Extended",
     "code": "class Person {\n  constructor(first, last) {\n    this.first = first;\n    this.last = last;\n  }\n\n  toString() {\n    return `${this.first} ${this.last}`;\n  }\n}\n\nclass SuperPerson extends Person {\n  constructor(first, last, power) {\n    super(first, last);\n    this.superPower = power;\n  }\n\n  toString() {\n    return `${super()} with the power of ${this.superPower}`;\n  }\n}\n\nlet brian = new SuperPerson('Brian', 'Genisio', 'code');\n\nconsole.log(brian.toString());",
     "intro": "<p>Classes can be extended using the <code>extends</code> keyword.  Methods can be overridden by re-defining them but the parent implementation can be called with <code>super()</code>;</p>\n<pre><code class=\"lang-javascript\"><span class=\"hljs-keyword\">class</span> SuperPerson extends Person {\n  constructor(first, last, power) {\n    super(first, last);\n    <span class=\"hljs-keyword\">this</span>.superPower = power;\n  }\n\n  toString() {\n    <span class=\"hljs-keyword\">return</span> `${super()} <span class=\"hljs-keyword\">with</span> the power of ${<span class=\"hljs-keyword\">this</span>.superPower}`;\n  }\n}\n</code></pre>\n<h3 id=\"give-it-a-try\">Give it a try</h3>\n"
 };
