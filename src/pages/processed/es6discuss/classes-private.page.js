c9721caa3386ea0d8563e4e52dcb710d7a4af9b1
Brian Genisio <BrianGenisio@Gmail.com>
2014-11-11 22:07:50 -0500

    Adding maps/sets and private properties

---
+++ src/pages/processed/es6discuss/classes-private.page.js
@@ -0,0 +1,6 @@
+export var page = {
+    "title": "Private Properties in Classes",
+    "heading": "Private Properties in Classes",
+    "code": "let priv = new WeakMap();\n\nclass Person {\n  constructor(first, last) {\n    priv.set(this, {first, last});\n  }\n\n  get first() { return priv.get(this).first; }\n  get last() { return priv.get(this).last; }\n}\n\n// Later, in another module\nvar brian = new Person('Brian', 'Genisio');\n\nconsole.log(brian.first, brian.last);",
+    "intro": ""
+};
