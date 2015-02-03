1904bdfe6a7b758cf378c2af26e3f706318c3354
Miguel Castillo <manchagnu@gmail.com>
2015-02-02 21:55:15 -0500

    Updates to use bit-imports with 6to5

--- site/src/editor/string-compiler.js
+++ site/src/editor/string-compiler.js
@@ -1,22 +1,41 @@
 class StringCompiler {
   transpile(code, moduleName) {
+    return callImporterCompiler(code, moduleName);
+//    return callTraceurCompiler(code, moduleName);
+  }
+}
+
+
+function callImporterCompiler(code) {
+    return importer.transform(code)
+      .then(function transpileSuccess(transpiled) {
+        let result =
+          `(function() {
+          ${ transpiled }
+          })();`;
+        return result;
+      }, function transpileError(err) {
+        console.log(err.message);
+        return err;
+      });
+}
+
+
+function callTraceurCompiler(code, moduleName) {
     let traceurCompiler = new traceur.Compiler({
       moduleName: moduleName,
       experimental: true,
       freeVariableChecker: true
     });
-    
+
     let transpiled = traceurCompiler.compile(code);
 
-    let result = 
-`(function() { 
-
-${ transpiled } 
-
-})();`;
+    let result =
+      `(function() {
+      ${ transpiled }
+      })();`;
 
     return result;
-  }
 }
 
 export default StringCompiler;
