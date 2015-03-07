f244ff180e42c666f796bc20f96ff9a361f4df44
Brian Genisio <BrianGenisio@Gmail.com>
2015-03-06 23:15:48 -0500

    Changing the text to say that it is using Babel

--- site/src/pages/markdown/about.md
+++ site/src/pages/markdown/about.md
@@ -4,6 +4,6 @@ heading: About LearnHarmony.org
 hideEditor: true
 ---
 
-This site was created by [Brian Genisio](http://briangenisio.com) as a mechanism to learn ES6 and teach others about it.  The code for this site is written in ES6 using Traceur and it is available on [Github](http://www.github.com/BrianGenisio/LearnHarmony)
+This site was created by [Brian Genisio](http://briangenisio.com) and [Miguel Castillo](https://github.com/MiguelCastillo) as a mechanism to learn ES6 and teach others about it.  The code for this site is written in ES6 using [Babel](http://babeljs.io) and it is available on [Github](http://www.github.com/BrianGenisio/LearnHarmony)
 
 Have Fun!
--- site/src/pages/processed/home.page.js
+++ site/src/pages/processed/home.page.js
@@ -7,5 +7,5 @@ export var page = {
     "heading": "Learn ES6 (Harmony) Now!",
     "hideComments": true,
     "code": "let futureJavascript = () => {\n    let ES6 = 'awesome';\n    console.log(`The future of JavaScript is ${ES6}`);\n};\n\nfutureJavascript();",
-    "intro": "<p>Welcome to LearnHarmony.org.  This site is designed to help you learn the new features of ECMAScript (primarily ES6 for now) through examples and exploration.  This site is in the early phases, so for now, just play along in the editor.  It uses Traceur, so any features that Traceur supports should work here.  Write to the console, have some fun.</p>\n<p>Over the next several weeks, I will start to put up micro-lessons designed to teach individual features.  Come back often! I expect it to be fun.</p>\n"
+    "intro": "<p>Welcome to LearnHarmony.org.  This site is designed to help you learn the new features of ECMAScript (primarily ES6 for now) through examples and exploration.  This site is in the early phases, so for now, just play along in the editor.  It uses <a href=\"http://babeljs.io\">Babel</a>, so any features that Babel supports should work here.  Write to the console, have some fun.</p>\n<p>Over the next several weeks, I will start to put up micro-lessons designed to teach individual features.  Come back often! I expect it to be fun.</p>\n"
 };
