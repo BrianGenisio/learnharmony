3e2ded64fae3d502c0b56eb51d7cf123623a5953
Brian Genisio <BrianGenisio@Gmail.com>
2015-06-19 21:31:15 -0400

    giving credit to bit-imports

--- site/src/pages/processed/about.page.js
+++ site/src/pages/processed/about.page.js
@@ -2,5 +2,5 @@ export var page = {
     "title": "About",
     "heading": "About LearnHarmony.org",
     "hideEditor": true,
-    "intro": "<p>This site was created by <a href=\"http://briangenisio.com\">Brian Genisio</a> and <a href=\"https://github.com/MiguelCastillo\">Miguel Castillo</a> as a mechanism to learn ES6 and teach others about it.  The code for this site is written in ES6 using <a href=\"http://babeljs.io\">Babel</a> and it is available on <a href=\"http://www.github.com/BrianGenisio/LearnHarmony\">Github</a></p>\n<p>Have Fun!</p>\n"
+    "intro": "<p>This site was created by <a href=\"http://briangenisio.com\">Brian Genisio</a> and <a href=\"https://github.com/MiguelCastillo\">Miguel Castillo</a> as a mechanism to learn ES6 and teach others about it.  The code for this site is written in ES6 using <a href=\"http://babeljs.io\">Babel</a> for transpiling in the browser and <a href=\"http://github.com/miguelcastillo/bit-imports\">bit imports</a> for the module loader.  The source is available on <a href=\"http://www.github.com/BrianGenisio/LearnHarmony\">Github</a></p>\n<p>Have Fun!</p>\n"
 };
