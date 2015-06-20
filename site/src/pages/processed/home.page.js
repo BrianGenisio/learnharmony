dfd90394d75ee4788cde34840667b4236c8cae87
Brian Genisio <BrianGenisio@Gmail.com>
2015-06-19 21:25:37 -0400

    Cleanup

--- site/src/pages/processed/home.page.js
+++ site/src/pages/processed/home.page.js
@@ -4,8 +4,8 @@ export var page = {
     "first": true,
     "next": "lessons/arrow-functions",
     "nextText": "Let's get started with our first lession -- Arrow Functions",
-    "heading": "Learn ES6 (Harmony) Now!",
+    "heading": "Learn ES2015 (ES6/Harmony) Now!",
     "hideComments": true,
-    "code": "let futureJavascript = () => {\n    let ES6 = 'awesome';\n    console.log(`The future of JavaScript is ${ES6}`);\n};\n\nfutureJavascript();",
-    "intro": "<p>Welcome to LearnHarmony.org.  This site is designed to help you learn the new features of ECMAScript (primarily ES6 for now) through examples and exploration.  This site is in the early phases, so for now, just play along in the editor.  It uses <a href=\"http://babeljs.io\">Babel</a>, so any features that Babel supports should work here.  Write to the console, have some fun.</p>\n<p>Over the next several weeks, I will start to put up micro-lessons designed to teach individual features.  Come back often! I expect it to be fun.</p>\n"
+    "code": "let futureJavascript = () => {\n    let ES2015 = 'awesome';\n    console.log(`The future of JavaScript is ${ES2015}`);\n};\n\nfutureJavascript();",
+    "intro": "<p>Welcome to LearnHarmony.org.  This site is designed to help you learn the new features of ECMAScript (primarily ES2015 for now) through examples and exploration. Write to the console, have some fun.</p>\n<p>There are several micro-lessons about all of the new features in JavaScript.  Have fun!</p>\n"
 };
