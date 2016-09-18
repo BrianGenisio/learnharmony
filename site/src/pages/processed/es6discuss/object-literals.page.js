23a8e23a0fc28c589c570455981b12d688a13715
Brian Genisio <briangenisio@gmail.org>
2016-09-17 22:05:06 -0400

    moving the content pipeline a single content file

--- site/src/pages/processed/es6discuss/object-literals.page.js
+++
@@ -1,8 +0,0 @@
-export var page = {
-    "title": "Object Literals",
-    "next": "es6discuss/default-values",
-    "nextText": "Default Values",
-    "heading": "Object Literals",
-    "code": "let key = \"someKey\";\nlet value = 55;\n\nlet obj = { \n  value,\n  [key + ' times 2']: value,\n  toString() {\n    return `The value is ${this.value}`;\n  }\n};\n\nconsole.log(obj);\nconsole.log(obj.toString());",
-    "intro": ""
-};
--- site/src/pages/processed/lessons/default-parameters.page.js
+++
@@ -1,9 +0,0 @@
-export var page = {
-    "title": "Default Parameters",
-    "navGroup": ".lessons",
-    "next": "lessons/block-scope-let",
-    "nextText": "Next, let's find out two new ways to declare variables.",
-    "heading": "Default Parameters",
-    "code": "function logDefaults(a = 1, b = 2, c = 3) {\n    console.log({a, b, c});\n}\n\nlogDefaults();\n\nfunction logDestructuredDefaults({a:A = 1, b:B, c:C = 3}) {\n    console.log({A, B, C});\n}\n\nlogDestructuredDefaults({b: 2});\n\nlet {a=1, b=2, c=3} = {};\nconsole.log({a, b, c});",
-    "intro": "<p>You have probably wanted it for a long time.  FINALLY!  You can define default values for parameters. Defaults can be assigned to any parameter. You can even assign default values to destructured values (both as function parameters and non-function destructuring).</p>\n<h3 id=\"give-it-a-try\">Give it a try</h3>\n"
-};
