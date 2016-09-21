1b04792a46b4d726a9a489ba418ba95a337c0ca1
Miguel Castillo <mcastillo@symphono.com>
2016-09-20 22:01:22 -0400

    cleaned up a lil of bit-imports config

--- src/import.config.js
+++ src/import.config.js
@@ -1,15 +1,11 @@
 import bitimports from 'bit-imports/browser';
 import babel from 'babel-bits';
 
-var System = (function() {
-  var importer = bitimports.config({
-    plugins: [{
-      transform: babel
-    }]
-  });
-
-  return importer;
-})();
+var System = bitimports.config({
+  plugins: [{
+    transform: babel
+  }]
+});
 
 global.System = System;
 export default System;
