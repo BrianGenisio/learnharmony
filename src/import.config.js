46a46c0efa4c5431bd811c7de81746c72bdc54ba
Miguel Castillo <mcastillo@symphono.com>
2016-09-20 21:54:39 -0400

    Adding bit-imports configuration with babel-bits and browser runnable bit-imports

--- src/import.config.js
+++ src/import.config.js
@@ -1,29 +1,12 @@
-import bitimports from 'bit-imports/dist/bit-imports';
+import bitimports from 'bit-imports/browser';
+import babel from 'babel-bits';
 
 var System = (function() {
   var importer = bitimports.config({
-    "paths": {
-      "babel": "node_modules/babel-bits/dist/index.min.js"
-    },
-    "transforms": [
-      {
-        name: "ignore",
-        handler: ignore,
-        ignore: ["babel"]
-      }, {
-        name: "babel"
-      }
-    ]
+    plugins: [{
+      transform: babel
+    }]
   });
-
-  /**
-   * Simple filter for excluding particular modules from being processed by the transformation pipeline.
-   */
-  
-  function ignore(moduleMeta) {
-    var ignoreList = this.ignore;
-    return !(ignoreList && ignoreList.length && ignoreList.indexOf(moduleMeta.name) !== -1);
-  }
 
   return importer;
 })();
