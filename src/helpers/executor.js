5858669e64cbcadf1361e9f9c332f38f980e70d5
Miguel Castillo <mcastillo@symphono.com>
2016-09-20 22:17:15 -0400

    Fixed executioner so that the module loader can register and import modules manually

--- src/helpers/executor.js
+++ src/helpers/executor.js
@@ -7,8 +7,8 @@ const ev = 'ev', al = 'al';
 const evil = window[ev+al];
 
 function execute(code, moduleName) {
-    System.register(moduleName, [], function() { return evil(code); });
-    return System.import(moduleName);
+    System.register(moduleName, () => evil(code));
+    return System.import(moduleName).then(mod => mod());
 }
 
 export {execute};
